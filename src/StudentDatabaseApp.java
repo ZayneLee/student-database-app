a47ec890529ce5282d1401c98eb70d13158a3743
Zayne <star1582@naver.com>
2024-08-25 09:54:06 +0100

    Creating the outline #3

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,5 +1,7 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        // Ask how many new users we want to add
 
+        // Create n number of new students
     }
 }
