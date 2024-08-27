136bfc4b2a1c427552cbcbf88be0e77dff66defe
Zayne <star1582@naver.com>
2024-08-27 09:14:12 +0100

    Initializing the student constructor #5

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
@@ -8,6 +10,20 @@ public class Student {
     private int costOfCourse = 600;
 
     // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+
+    }
 
     // Generate an ID
 
