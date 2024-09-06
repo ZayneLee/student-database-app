1d67b1db6157a0050b8d9eb946ee3e9143d14bad
Zayne <star1582@naver.com>
2024-09-06 16:07:44 +0900

    Ask how many new students we want to add

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        // Ask how many new users we want to add
-        Student s1 = new Student();
-        s1.enroll();
-        s1.payTuition();
-        System.out.println(s1.toString());
+        // Ask how many new students we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
         // Create n number of new students
+        for(int n = 0; n < numOfStudents; n++) {
+            studnets[n] = new Student();
+            studnets[n].enroll();
+            studnets[n].payTuition();
+            System.out.println(s1.toString());
+        }
+
+        System.out.println(students[0].toString());
+        System.out.println(students[1].toString());
+        System.out.println(students[2].toString());
     }
 }
